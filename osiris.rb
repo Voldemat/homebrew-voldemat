63a2db2f9da9cf28c12d6f2b68bbe5854cce3c33
Vladimir <vladimirdev635@gmail.com>
2023-02-20 00:40:44 +0500

    Brew formula update for osiris version 0.7.4

--- osiris.rb
+++ osiris.rb
@@ -5,11 +5,11 @@
 class Osiris < Formula
   desc ""
   homepage "https://github.com/Voldemat/osiris"
-  version "0.7.3"
+  version "0.7.4"
 
   on_macos do
-    url "https://github.com/Voldemat/osiris/releases/download/0.7.3/osiris_0.7.3_darwin_all.tar.gz"
-    sha256 "957bccaf4183f8370f490130a6d9dc1c6b701d2bcd17aed1cba3a5935247f584"
+    url "https://github.com/Voldemat/osiris/releases/download/0.7.4/osiris_0.7.4_darwin_all.tar.gz"
+    sha256 "76d77f4b4e62c355b8dbbccfa899f450921e7d070d4b425fda8a251d783ad636"
 
     def install
       bin.install "osiris"
@@ -17,17 +17,17 @@ class Osiris < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/Voldemat/osiris/releases/download/0.7.3/osiris_0.7.3_linux_amd64.tar.gz"
-      sha256 "95ff2c6e687501112234451c7affb7b974a1613473f91dcaff5c478ab7bf3f45"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/Voldemat/osiris/releases/download/0.7.4/osiris_0.7.4_linux_arm64.tar.gz"
+      sha256 "5e686e0c4fc986218c892b7db44a6ad4ec5fd7a29d7976ac7a98e42228796ba2"
 
       def install
         bin.install "osiris"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Voldemat/osiris/releases/download/0.7.3/osiris_0.7.3_linux_arm64.tar.gz"
-      sha256 "c45207cfcd3f4a6bb4c4a7b01b3482ec460b55602a8fbe63de993cb8594f096f"
+    if Hardware::CPU.intel?
+      url "https://github.com/Voldemat/osiris/releases/download/0.7.4/osiris_0.7.4_linux_amd64.tar.gz"
+      sha256 "a8590f6277277248327855a5653eb8f6278163a4337dfe81be22687691bf5597"
 
       def install
         bin.install "osiris"
